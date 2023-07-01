a8cb28ca31a91a8312f5483d0a9f9da4ed922e07
suny06a <sunny.tomar.ug20@nsut.ac.in>
2023-07-01 18:42:25 +0530

    adding and fetching notes

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const notesSchema = new mongoose.Schema({
+   user:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'User'
+   },
    title: {
     type:String,
     required:true
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,7 +1,36 @@
 const express=require('express');
 const router=express.Router();
-router.get('/',(req,res)=>{
-   
-    res.json([])
+const fetchUser=require('../middleware/fetchUser')
+const Notes=require('../models/Notes')
+const { body, validationResult } = require("express-validator");
+//create new note
+router.post('/addnote',fetchUser,[
+
+],async (req,res)=>{
+    body("title").isLength({min:5}).withMessage(" title must be min 5 "),
+    body("description").isLength({min:10}).withMessage("mini 10")
+    
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.send({ errors: result.array() });
+    }
+    try{
+      const {title,description,tag}=req.body;
+      const note= new Notes({
+        title,description,tag,user:req.user.id
+      })
+      const saveNote=await note.save()
+      res.json(saveNote);
+    }
+    catch (error) {
+        console.error(error.message);
+        res.json("some error occured");
+      }
+})
+
+//get all notes
+router.get('/fetchnotes',fetchUser, async (req,res)=>{
+   const notes=await Notes.find({user:req.user.id});
+   res.json(notes)
 })
 module.exports=router
