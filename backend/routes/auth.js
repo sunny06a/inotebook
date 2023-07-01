71625c1f997ff22e8221b34fc9ea32a0bdb8c920
suny06a <sunny.tomar.ug20@nsut.ac.in>
2023-07-01 11:49:16 +0530

    express-validator added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,18 @@
 const express=require('express');
 const User=require('../models/User');
 const router=express.Router();
-router.post('/',(req,res)=>{
+const { body, validationResult } = require('express-validator');
+router.post('/',[
+    body('name').isLength({ min: 4 }).withMessage("min. 4 chracter"),
+    body('email').isEmail().withMessage("enter valid email"),
+    body('password').isLength({ min: 8 }).withMessage("min 8 character"),
+],(req,res)=>{
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.send({ errors: result.array() });
+    }
     
-    User.create(req.body);
+    User.create(req.body) //  to send message when duplicate email .then(user=>res.json(user)).catch(err=>console.log(err));
     res.send(req.body)
 })
 module.exports=router
