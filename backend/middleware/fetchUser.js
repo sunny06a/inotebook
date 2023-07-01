912364c322b7bed0383581f928b459c17052a63b
suny06a <sunny.tomar.ug20@nsut.ac.in>
2023-07-01 18:17:36 +0530

    fetchUser middleware

---
+++ backend/middleware/fetchUser.js
@@ -0,0 +1,18 @@
+var jwt=require('jsonwebtoken');
+
+const fetchUser=(req,res,next)=>{
+    //get user id from the jwt token
+    const token=req.header('auth_token');
+    if(!token){
+        res.json("no token availble");
+    }
+    try{
+    const data=jwt.verify(token,"sunny123");
+    req.user=data.user;
+    next();
+    }catch(error){
+        res.send(401);
+    }
+}
+
+module.exports=fetchUser;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,50 +1,101 @@
-const express=require('express');
-const User=require('../models/User');
-const router=express.Router();
-const { body, validationResult } = require('express-validator');
-const bcrypt=require('bcryptjs'); //password hashing
-const jwt=require('jsonwebtoken')
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs"); //password hashing
+const jwt = require("jsonwebtoken");
+const fetchUser=require('../middleware/fetchUser')
 
-
-router.post('/createuser',[
+router.post(
+  "/createuser",
+  [
     //check for validation
-    body('name').isLength({ min: 4 }).withMessage("min. 4 chracter"),
-    body('email').isEmail().withMessage("enter valid email"),
-    body('password').isLength({ min: 8 }).withMessage("min 8 character"),
-
-],async (req,res)=>{
+    body("name").isLength({ min: 4 }).withMessage("min. 4 chracter"),
+    body("email").isEmail().withMessage("enter valid email"),
+    body("password").isLength({ min: 8 }).withMessage("min 8 character"),
+  ],
+  async (req, res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
       return res.send({ errors: result.array() });
     }
     //check unique email in database
-    try{
-    let user=await User.findOne({email:req.body.email});
-    if(user){
-        return res.json('email already exsist')
-    }
-    else{
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.json("email already exsist");
+      } else {
         //password hashing
-        const salt= await bcrypt.genSalt(10);
-        const secPass= await bcrypt.hash(req.body.password,salt);
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
         user = await User.create({
-            name:req.body.name,
-            email:req.body.email,
-            password:secPass    
+          name: req.body.name,
+          email: req.body.email,
+          password: secPass,
         });
+      }
+      //for sign in web token
+      const data = {
+        user: {
+          id: User.id,
+        },
+      };
+      const auth_token = jwt.sign(data, "sunny123");
+      res.json({ auth_token: auth_token });
+    } catch (error) {
+      console.error(error.message);
+      res.json("some error occured");
     }
-    //for sign in web token
-    const data={
-        user:{
-            id:User.id
-        }
-    }
-    const auth_token=jwt.sign(data,'sunny123')
-    res.json({authtoken:auth_token});  
-     } catch(error){
-        console.error(error.message);
-        res.json("some error occured");
-     }
+  }
+);
 
-})
-module.exports=router
+router.post("/login",
+  [
+    //check for validation
+    body("email").isEmail().withMessage("enter valid email"),
+    body("password").exists().withMessage("cant be blank"),
+  ],
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.send({ errors: result.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email: email });
+      if (!user) {
+        return res.json("Enter correct details");
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.json("Enter correct details");
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const auth_token = jwt.sign(data,"sunny123");
+      res.send({ auth_token });
+    } catch {
+      console.error(error.message);
+      res.json("some error occured");
+    }
+  }
+);
+
+//route 3 get looging details of user
+
+router.post("/getuser", fetchUser,async (req, res) => {
+  try {
+    const user_id=req.user;
+    console.log(user_id)
+    const user = await User.findById(user_id.id).select("-passowrd");
+    res.json(user);
+  } catch {
+    console.error(error.message);
+    res.json("some error occured");
+  }
+});
+module.exports = router;
